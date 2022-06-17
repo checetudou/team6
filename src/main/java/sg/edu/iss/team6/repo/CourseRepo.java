237d9feab6a281647cf879376f68785620df9268
xingjietan <xingjietan@macbook-pro-2>
2022-06-17 19:22:48 +0800

    lecturerController v0.1

--- src/main/java/sg/edu/iss/team6/repo/CourseRepo.java
+++ src/main/java/sg/edu/iss/team6/repo/CourseRepo.java
@@ -1,11 +1,20 @@
 package sg.edu.iss.team6.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import sg.edu.iss.team6.model.Courses;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sg.edu.iss.team6.model.*;
 
 import java.util.ArrayList;
 
-public interface CourseRepo extends JpaRepository<Courses,String> {
-    ArrayList<Courses> findCoursesByCourseId(String courseId);
-    //@Query
+public interface CourseRepo extends JpaRepository<Courses,Integer> {
+    @Query("Select c from Courses WHERE c.CourseId = :cid")
+    ArrayList<Courses> findCoursesByCourseId(@Param("cid") Integer cid);
+
+    @Query("Select lc from LectureCanTeach WHERE lc.LecturerId = :lid")
+	ArrayList<LectureCanTeach> findCoursesByLecturerId(@Param("lid") Integer lecturerId);
+    
+    @Query("Select sc from StudentAttendCourse WHERE sc.studentId = :sid")
+	ArrayList<StudentAttendCourse> findCoursesByStudentId(@Param("sid") Integer lecturerId);
 }
