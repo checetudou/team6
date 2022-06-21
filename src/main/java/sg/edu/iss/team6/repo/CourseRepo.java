3bde6d763920dde634c476069f394a225679e6a9
halim <halim261@gmail.com>
2022-06-21 21:24:04 +0800

    repo package cleanup

--- src/main/java/sg/edu/iss/team6/repo/CourseRepo.java
+++ src/main/java/sg/edu/iss/team6/repo/CourseRepo.java
@@ -1,50 +1,24 @@
 package sg.edu.iss.team6.repo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import sg.edu.iss.team6.model.Courses;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import sg.edu.iss.team6.model.*;
-
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import sg.edu.iss.team6.model.Courses;
-
-
-
-import java.util.List;
-import java.util.Optional;
-
-import java.util.List;
-import java.util.Optional;
+import sg.edu.iss.team6.model.StudentAttendCourse;
 
 
 @Repository
 public interface CourseRepo extends JpaRepository<Courses,String> {
     
-	@Query("select c from Courses c where c.courseId like %?1%")
-	List<Courses> findCoursesByCourseId(String courseId);
-    
 //	@Query("select c from Courses c where c.courseName like %?1%")
 //    List<Courses> findCoursesByCourseName(String courseName);
-		
-	@Query("select c.size from Courses c where c.courseId like %?1%")
-	Courses getAllowedSize (int count);
 
-	Optional<Courses> findByName(String name);
-
-    @Query("Select c from Courses c WHERE c.courseId = :cid")
-    ArrayList<Courses> findCoursesByCourseId(@Param("cid") String cid);
-//
 //    @Query("Select lc from LectureCanTeach lc WHERE lc.lecturers.lecturerId = :lid")
 //	ArrayList<LectureCanTeach> findCoursesByLecturerId(@Param("lid") String lecturerId);
     
--- src/main/java/sg/edu/iss/team6/repo/UserRepo.java
+++ src/main/java/sg/edu/iss/team6/repo/UserRepo.java
@@ -1,17 +1,15 @@
 package sg.edu.iss.team6.repo;
 
-import java.util.ArrayList;
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import sg.edu.iss.team6.model.Lecturers;
 import sg.edu.iss.team6.model.Students;
 
-
+@Repository
 public interface UserRepo extends JpaRepository<Lecturers, String> {
 
 	@Query("SELECT s FROM Students s  WHERE s.studentId=:stuid")
