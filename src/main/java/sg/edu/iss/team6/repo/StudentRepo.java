14d488ecfba2aa36947efa953d514282638699f1
Nina Lim <e0008256@u.nus.edu>
2022-06-21 22:56:27 +0800

    22:56

--- src/main/java/sg/edu/iss/team6/repo/StudentRepo.java
+++ src/main/java/sg/edu/iss/team6/repo/StudentRepo.java
@@ -25,9 +25,9 @@ import java.util.List;
 
 public interface StudentRepo extends JpaRepository<Students,String> {
       
-    @Query("select c from Courses c where c not in (select c from StudentAttendCourse sac,Courses c where sac.students = :sid and sac.courses.courseId=c.courseId)")
-    ArrayList<Courses> findAvailableCoursesByStudentId(@Param("sid") String studentId);
-
+//    @Query("select c from Courses c where c not in (select c from StudentAttendCourse sac,Courses c where sac.students = :sid and sac.courses.courseId=c.courseId)")
+//    ArrayList<Courses> findAvailableCoursesByStudentId(@Param("sid") String studentId);
+//
 
 //       @Query("Select c.size from Courses c WHERE c.courseId = :cid")
 //       int getCourseCapacityById(String courseId);
@@ -40,27 +40,26 @@ public interface StudentRepo extends JpaRepository<Students,String> {
 
     ArrayList<Students> findstudentsByStudentId(String studentId);
     
-	@Query("select c from Courses c where c in (select c from StudentAttendCourse sac,Courses c where sac.students = :sid and sac.courses.courseId=c.courseId)")
-    StudentAttendCourse findStudentByStudentId(String studentId);
-	
+//	@Query("select c from Courses c where c in (select c from StudentAttendCourse sac,Courses c where sac.students = :sid and sac.courses.courseId=c.courseId)")
+//    StudentAttendCourse findStudentByStudentId(String studentId);
+//	
+//       
+//    @Query("Select c.size from Courses c WHERE c.courseId == :cid")
+//    int getCourseCapacityById(String courseId);
+//       
+//    @Query("Select c.actualEnroll from Courses c WHERE c.courseId == :cid")
+//    int getActualEnrolledById(String courseId);
+//       
+//    @Query("Select c from Courses c WHERE c.courseId == :cid")
+//    Courses findCourseByCourseId(String courseId);
+//
+//    @Query("select c from Courses c where c not in (select c from StudentAttendCourse sac,Courses c where sac.students = :sid and sac.courses.courseId=c.courseId)")
+//    ArrayList<Courses> findAvailableCoursesByStudentId(@Param("sid") String studentId);
        
-       @Query("Select c.size from Courses c WHERE c.courseId == :cid")
-       int getCourseCapacityById(String courseId);
-       
-       @Query("Select c.actualEnroll from Courses c WHERE c.courseId == :cid")
-       int getActualEnrolledById(String courseId);
-       
-       @Query("Select c from Courses c WHERE c.courseId == :cid")
-       Courses findCourseByCourseId(String courseId);
 
-       
-       @Query("select c from Courses c where c not in (select c from StudentAttendCourse sac,Courses c where sac.students = :sid and sac.courses.courseId=c.courseId)")
-       ArrayList<Courses> findAvailableCoursesByStudentId(@Param("sid") String studentId);
-       
-
-	@Query("select s from Students s where s.Id or s.Name like %?1%")
-	Courses getCurrentSize (int count);
-	
+//	@Query("select s from Students s where s.Id or s.Name like %?1%")
+//	Courses getCurrentSize (int count);
+//	
 	@Query("select s from Students s where s.Id like %?1%")
 	ArrayList<Students> getStudentProfileById(String id);
 	
