22162f4eadf360a67bd0d5115a7b5f800b5a8b19
viviyma <s10142908@gmail.com>
2022-06-23 00:03:46 +0800

    StudentController Retouch and add views for grade page and enrollment page

--- src/main/java/sg/edu/iss/team6/services/StudentAttendCourseService.java
+++ src/main/java/sg/edu/iss/team6/services/StudentAttendCourseService.java
@@ -1,9 +1,15 @@
 package sg.edu.iss.team6.services;
 
+import java.util.ArrayList;
+
+import sg.edu.iss.team6.model.Courses;
 import sg.edu.iss.team6.model.StudentAttendCourse;
 
 public interface StudentAttendCourseService {
 	
 	StudentAttendCourse createStudentAttendCourse(StudentAttendCourse SAC);
+
+	ArrayList<StudentAttendCourse> findStudentAttendCourseByStudentId(String StudentId);
+
 
 }
--- src/main/java/sg/edu/iss/team6/services/StudentAttendCourseServiceImpl.java
+++ src/main/java/sg/edu/iss/team6/services/StudentAttendCourseServiceImpl.java
@@ -1,10 +1,13 @@
 package sg.edu.iss.team6.services;
 
+import java.util.ArrayList;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import sg.edu.iss.team6.model.Courses;
 import sg.edu.iss.team6.model.StudentAttendCourse;
 import sg.edu.iss.team6.repo.StudentAttendCourseRepo;
 
@@ -21,5 +24,12 @@ public class StudentAttendCourseServiceImpl implements StudentAttendCourseServic
 	public StudentAttendCourse createStudentAttendCourse(StudentAttendCourse SAC) {
 		return SACrepo.saveAndFlush(SAC);
 	}
+	
+	@Override
+	@Transactional
+	public ArrayList<StudentAttendCourse> findStudentAttendCourseByStudentId(String studentId) {
+		return SACrepo.findStudentAttendCourseByStudentId(studentId);
+	}
 
 }
+
