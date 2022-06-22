f191a136791ac465002b4375d2b4e3b8f65a3f84
Nina Lim <e0008256@u.nus.edu>
2022-06-22 22:35:33 +0800

    admin addcourse view

--- src/main/java/sg/edu/iss/team6/repo/CourseRepo.java
+++ src/main/java/sg/edu/iss/team6/repo/CourseRepo.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -50,4 +51,7 @@ public interface CourseRepo extends JpaRepository<Courses,String> {
 	void updateCourse(Courses course);
 
 	List<Courses> getAllCourses(Pageable pageable);
+	
+	//Pageable firstPageWithTenElements = PageRequest.of(0, 10);
+	//https://www.baeldung.com/spring-data-jpa-pagination-sorting
 }
